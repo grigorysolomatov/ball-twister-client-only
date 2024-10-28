d7c65dc68171dda76a8142b2c7cceaea92ff9777
grigorysolomatov <30951147+grigorysolomatov@users.noreply.github.com>
2024-10-28 07:22:03 +0100

    Chapter 2

--- js/game-content.js
+++ js/game-content.js
@@ -157,9 +157,100 @@ export const gameContent = {
 		0: async ctx => {
 		    const {scene, height, width} = ctx;
 		    const choice = await scene.newMenu(0.5*width, 0.5*height, {
+			monday: 'Monday',
+			tuesday: 'Tuesday',
+			wednesday: 'Wednesday',
+			thursday: 'Thursday',
+			friday: 'Friday',
+			__space__: '',
 			'..': 'Back',
 		    });
 		    return choice;
+		},
+		monday: {
+		    0: levelTemplate({
+			content: [
+			    [0,0,0,0,0,0,0],
+			    [0,0,0,0,0,0,0],
+			    [0,0,1,1,1,0,0],
+			    [0,0,1,0,1,0,0],
+			    [0,0,1,1,1,0,0],
+			    [0,0,0,0,0,0,0],
+			    [0,0,0,0,0,0,0],
+			],
+			images: ['gray', 'gold'],
+			sensor: 'sensor',
+			eye: 'eye',
+			trofe: 'trofe',
+		    }),
+		},
+		tuesday: {
+		    0: levelTemplate({
+			content: [
+			    [0,0,0,0,0,0,0],
+			    [0,0,0,0,0,0,0],
+			    [0,1,1,1,1,1,0],
+			    [0,1,0,1,0,1,0],
+			    [0,1,1,1,1,1,0],
+			    [0,0,0,0,0,0,0],
+			    [0,0,0,0,0,0,0],
+			],
+			images: ['gray', 'gold'],
+			sensor: 'sensor',
+			eye: 'eye',
+			trofe: 'trofe',
+		    }),
+		},
+		wednesday: {
+		    0: levelTemplate({
+			content: [
+			    [0,0,0,0,0,0,0],
+			    [0,0,0,0,0,0,0],
+			    [1,1,1,1,1,1,1],
+			    [1,0,1,0,1,0,1],
+			    [1,1,1,1,1,1,1],
+			    [0,0,0,0,0,0,0],
+			    [0,0,0,0,0,0,0],
+			],
+			images: ['gray', 'gold'],
+			sensor: 'sensor',
+			eye: 'eye',
+			trofe: 'trofe',
+		    }),
+		},
+		thursday: {
+		    0: levelTemplate({
+			content: [
+			    [0,0,0,0,0,0,0],
+			    [0,1,1,1,1,1,0],
+			    [0,1,0,1,0,1,0],
+			    [0,1,1,1,1,1,0],
+			    [0,1,0,1,0,1,0],
+			    [0,1,1,1,1,1,0],
+			    [0,0,0,0,0,0,0],
+			],
+			images: ['gray', 'gold'],
+			sensor: 'sensor',
+			eye: 'eye',
+			trofe: 'trofe',
+		    }),
+		},
+		friday: {
+		    0: levelTemplate({
+			content: [
+			    [1,1,1,1,1,1,1],
+			    [1,0,1,0,1,0,1],
+			    [1,1,1,1,1,1,1],
+			    [1,0,1,0,1,0,1],
+			    [1,1,1,1,1,1,1],
+			    [1,0,1,0,1,0,1],
+			    [1,1,1,1,1,1,1],
+			],
+			images: ['gray', 'gold'],
+			sensor: 'sensor',
+			eye: 'eye',
+			trofe: 'trofe',
+		    }),
 		},
 	    },
 	},
