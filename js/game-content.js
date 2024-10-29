c5a06c4341c89756a53e02dd86abe67a1a0d14a0
grigorysolomatov <30951147+grigorysolomatov@users.noreply.github.com>
2024-10-29 11:38:19 +0100

    Chatper 5

--- js/game-content.js
+++ js/game-content.js
@@ -51,6 +51,7 @@ export const gameContent = {
 		    chapter2: 'Chapter 2',
 		    chapter3: 'Chapter 3',
 		    chapter4: 'Chapter 4',
+		    chapter5: 'Chapter 5',
 		    __space__: '',
 		    '..': 'Back',
 		});
@@ -448,6 +449,106 @@ export const gameContent = {
 			    [0,0,1,1,1,0,0],
 			    [0,1,0,1,0,1,0],
 			    [1,0,0,1,0,0,1],
+			],
+			images: ['gray', 'gold'],
+			sensor: 'sensor',
+			eye: 'eye',
+			trofe: 'trofe',
+		    }),
+		},
+	    },
+	    chapter5: {
+		0: async ctx => {
+		    const {scene, height, width} = ctx;
+		    const choice = await scene.newMenu(0.5*width, 0.5*height, {
+			monday: 'Monday',
+			tuesday: 'Tuesday',
+			wednesday: 'Wednesday',
+			thursday: 'Thursday',
+			friday: 'Friday',
+			__space__: '',
+			'..': 'Back',
+		    });
+		    return choice;
+		},
+		monday: {
+		    0: levelTemplate({
+			content: [
+			    [0,0,0,0,0,0,0],
+			    [0,0,0,0,0,0,0],
+			    [1,0,0,0,1,0,0],
+			    [0,1,0,1,0,1,0],
+			    [0,0,1,0,0,0,1],
+			    [0,0,0,0,0,0,0],
+			    [0,0,0,0,0,0,0],
+			],
+			images: ['gray', 'gold'],
+			sensor: 'sensor',
+			eye: 'eye',
+			trofe: 'trofe',
+		    }),
+		},
+		tuesday: {
+		    0: levelTemplate({
+			content: [
+			    [0,0,0,0,0,0,0],
+			    [1,1,1,1,1,1,1],
+			    [1,0,0,0,1,0,0],
+			    [0,1,0,1,0,1,0],
+			    [0,0,1,0,0,0,1],
+			    [1,1,1,1,1,1,1],
+			    [0,0,0,0,0,0,0],
+			],
+			images: ['gray', 'gold'],
+			sensor: 'sensor',
+			eye: 'eye',
+			trofe: 'trofe',
+		    }),
+		},
+		wednesday: {
+		    0: levelTemplate({
+			content: [
+			    [1,0,1,0,1,0,1],
+			    [0,1,0,1,0,1,0],
+			    [0,0,0,0,0,0,0],
+			    [1,1,1,1,1,1,1],
+			    [0,0,0,0,0,0,0],
+			    [0,1,0,1,0,1,0],
+			    [1,0,1,0,1,0,1],
+			],
+			images: ['gray', 'gold'],
+			sensor: 'sensor',
+			eye: 'eye',
+			trofe: 'trofe',
+		    }),
+		},
+		thursday: {
+		    0: levelTemplate({
+			content: [
+			    [1,0,1,0,1,0,1],
+			    [1,1,0,1,0,1,1],
+			    [1,0,1,0,1,0,1],
+			    [1,0,0,1,0,0,1],
+			    [1,0,1,0,1,0,1],
+			    [1,1,0,1,0,1,1],
+			    [1,0,1,0,1,0,1],
+			],
+			images: ['gray', 'gold'],
+			sensor: 'sensor',
+			eye: 'eye',
+			trofe: 'trofe',
+		    }),
+		},
+		friday: {
+		    0: levelTemplate({
+			content: [
+			    [1,0,1,1,1,0,1],
+			    [0,1,0,0,0,1,0],
+			    [1,0,1,1,1,0,1],
+			    [1,0,1,0,1,0,1],
+			    [1,0,1,1,1,0,1],
+			    [0,1,0,0,0,1,0],
+			    [1,0,1,1,1,0,1],
 			],
 			images: ['gray', 'gold'],
 			sensor: 'sensor',
