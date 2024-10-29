2b40abea87a98775640e1604f995144d086ecb06
grigorysolomatov <30951147+grigorysolomatov@users.noreply.github.com>
2024-10-29 15:56:01 +0100

    Learning poem

--- js/game-content.js
+++ js/game-content.js
@@ -561,9 +561,36 @@ export const gameContent = {
 	learn: {
 	    0: async ctx => {
 		const {scene, height, width} = ctx;
-		const choice = await scene.newMenu(0.5*width, 0.5*height, {
+		
+		const text = scene.newText(
+		    0.5*width, 0.5*height,
+		    [
+			'Restore the pattern',
+			'by twisting the balls',
+			'',
+			'You tap or you hold:',
+			'those are the controls',
+			'',
+			'Design your solution',
+			'in your unique way',
+			'',
+			'Success is contingent',
+			'on patience and brain',
+		    ].join('\n'), {align: 'center'}).setOrigin(0.5).setScale(0.8);
+		await text.tween({
+		    x: {from: 3*text.x, to: text.x},
+		    duration: 1000,
+		    ease: 'Cubic.easeOut',
+		});
+		const choice = await scene.newMenu(0.5*width, 0.9*height, {
 		    '..': 'Back',
 		});
+		await text.tween({
+		    x: 3*text.x,
+		    duration: 1000,
+		    ease: 'Cubic.easeIn',
+		}); text.destroy();
+		
 		return choice;
 	    },
 	},
